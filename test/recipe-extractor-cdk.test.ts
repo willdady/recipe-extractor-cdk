8458caa63d6c5995d1dec0944a9752e6c0cbe2f0
Will Dady <willdady@gmail.com>
2024-05-13 08:32:06 +1000

    Initial commit

---
+++ test/recipe-extractor-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RecipeExtractorCdk from '../lib/recipe-extractor-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/recipe-extractor-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RecipeExtractorCdk.RecipeExtractorCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
